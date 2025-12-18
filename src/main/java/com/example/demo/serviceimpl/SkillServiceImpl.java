c22dcc6a92566afee4d80bc92dcb4d0f08ac3e3d
bala-amypo <bala@amypo.in>
2025-12-18 08:08:44 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/SkillServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/SkillServiceImpl.java
@@ -5,28 +5,22 @@ import com.example.demo.service.SkillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
 @Service
 public class SkillServiceImpl implements SkillService {
-
     @Autowired
     private SkillRepository repository;
-
     @Override
     public Skill addSkill(Skill skill) {
         return repository.save(skill);
     }
-
     @Override
     public Skill getSkillById(Long id) {
         return repository.findById(id).orElseThrow();
     }
-
     @Override
     public List<Skill> getAllSkills() {
         return repository.findAll();
     }
-
     @Override
     public Skill deactivateSkill(Long id) {
         Skill skill = repository.findById(id).orElseThrow();
--- src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
@@ -1,29 +1,22 @@
 package com.example.demo.service.impl;
-
 import com.example.demo.entity.StudentProfile;
 import com.example.demo.repository.StudentProfileRepository;
 import com.example.demo.service.StudentProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 @Service
 public class StudentProfileServiceImpl implements StudentProfileService {
-
     @Autowired
     private StudentProfileRepository repository;
-
     @Override
     public StudentProfile createProfile(StudentProfile profile) {
         return repository.save(profile);
     }
-
     @Override
     public StudentProfile getProfileById(Long id) {
         return repository.findById(id).orElseThrow();
     }
-
     @Override
     public List<StudentProfile> getAllProfiles() {
         return repository.findAll();
