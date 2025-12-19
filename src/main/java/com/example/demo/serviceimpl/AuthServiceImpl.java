7773d0a95d4e8a88bee5625237b4881572c3e1da
bala-amypo <bala@amypo.in>
2025-12-19 15:29:55 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/AuthServiceImpl.java
@@ -1,33 +1,54 @@
 package com.example.demo.service.impl;
+
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
+
 @Service
 public class AuthServiceImpl implements AuthService {
+
     @Autowired
     private UserRepository userRepository;
+
     @Override
     public User register(User user) {
         return userRepository.save(user);
     }
+
     @Override
     public User login(String email, String password) {
+
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED,
+                        "User not found"
+                ));
 
         if (!user.getPassword().equals(password)) {
-            throw new RuntimeException("Invalid credentials");
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "Invalid credentials"
+            );
         }
 
         return user;
     }
+
     @Override
     public User getUserById(Long id) {
-        return userRepository.findById(id).orElseThrow();
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "User not found"
+                ));
     }
+
     @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
