ceb902ef14fc6bc9a06f67fcb3e06722b3e57be1
bala-amypo <bala@amypo.in>
2025-12-20 14:44:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssessmentResultRepository.java
+++ src/main/java/com/example/demo/repository/AssessmentResultRepository.java
@@ -3,6 +3,13 @@ package com.example.demo.repository;
 import com.example.demo.entity.AssessmentResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AssessmentResultRepository
         extends JpaRepository<AssessmentResult, Long> {
+
+    Optional<AssessmentResult> findByStudentProfile_IdAndSkill_Id(
+            Long studentId,
+            Long skillId
+    );
 }
