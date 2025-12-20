8a0e2825fcfbf83b3b8667b2409f068b06fc3343
bala-amypo <bala@amypo.in>
2025-12-20 15:23:32 +0000

    User commit

--- src/main/java/com/example/demo/service/AssessmentResultService.java
+++ src/main/java/com/example/demo/service/AssessmentResultService.java
@@ -1,12 +1,9 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.AssessmentRequest;
 import com.example.demo.entity.AssessmentResult;
-
-import java.util.List;
 
 public interface AssessmentResultService {
 
-    AssessmentResult saveResult(AssessmentResult result);
-
-    List<AssessmentResult> getResultsByStudentId(Long studentId);
+    AssessmentResult saveAssessment(AssessmentRequest request);
 }
--- src/main/java/com/example/demo/serviceimpl/AssessmentResultServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/AssessmentResultServiceImpl.java
@@ -1,26 +1,26 @@
 package com.example.demo.serviceimpl;
 
+import com.example.demo.dto.AssessmentRequest;
 import com.example.demo.entity.AssessmentResult;
 import com.example.demo.repository.AssessmentResultRepository;
 import com.example.demo.service.AssessmentResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class AssessmentResultServiceImpl implements AssessmentResultService {
+public class AssessmentResultServiceImpl
+        implements AssessmentResultService {
 
     @Autowired
     private AssessmentResultRepository repository;
 
     @Override
-    public AssessmentResult saveResult(AssessmentResult result) {
+    public AssessmentResult saveAssessment(AssessmentRequest request) {
+        AssessmentResult result = new AssessmentResult();
+        result.setScoreObtained(request.getScoreObtained());
+        result.setStudentId(request.getStudentId());
+        result.setSkillId(request.getSkillId());
+        result.setSubject(request.getSubject());
         return repository.save(result);
-    }
-
-    @Override
-    public List<AssessmentResult> getResultsByStudentId(Long studentId) {
-        return repository.findByStudentId(studentId);
     }
 }
