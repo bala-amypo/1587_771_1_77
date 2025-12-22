3474a960fa92459c84a741e6f6f062fff18c5ae9
bala-amypo <bala@amypo.in>
2025-12-22 04:39:28 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/RecommendationServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/RecommendationServiceImpl.java
@@ -2,16 +2,18 @@ package com.example.demo.serviceimpl;
 
 import com.example.demo.entity.Priority;
 import com.example.demo.entity.SkillGapRecommendation;
+import com.example.demo.service.RecommendationService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RecommendationServiceImpl {
+public class RecommendationServiceImpl implements RecommendationService {
 
-    public SkillGapRecommendation buildRecommendation() {
+    @Override
+    public SkillGapRecommendation getRecommendation() {
         SkillGapRecommendation rec = new SkillGapRecommendation();
         rec.setSkillName("Spring Boot");
         rec.setPriority(Priority.HIGH);
-        rec.setRecommendation("Focus on REST APIs and JPA");
+        rec.setRecommendation("Focus on REST APIs, JPA, and Exception Handling");
         return rec;
     }
 }
