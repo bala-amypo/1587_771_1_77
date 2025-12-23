5af7b43b848d6655ce32568a28cf477b7564badb
bala-amypo <bala@amypo.in>
2025-12-23 04:38:03 +0000

    User commit

--- src/main/java/com/example/demo/exception/DuplicateResourceException.java
+++ src/main/java/com/example/demo/exception/DuplicateResourceException.java
@@ -2,12 +2,11 @@ package com.example.demo.exception;
 
 public class DuplicateResourceException extends RuntimeException {
     
-        public DuplicateResourceException(String message) {
-                super(message);
-                    }
+public DuplicateResourceException(String message) {
+super(message);
+ }
                         
-                            public DuplicateResourceException(String message, Throwable cause) {
-                                    super(message, cause);
-                                        }
-                                        }
-                                        
+public DuplicateResourceException(String message, Throwable cause) {
+super(message, cause);
+}
+}
