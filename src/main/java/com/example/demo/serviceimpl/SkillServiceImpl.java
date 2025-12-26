2077d19b6e8102bdfc7fde283051e9b41db5e25d
bala-amypo <bala@amypo.in>
2025-12-26 05:24:44 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/SkillServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/SkillServiceImpl.java
@@ -1,12 +1,13 @@
 package com.example.demo.serviceimpl;
 
 import com.example.demo.entity.Skill;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.SkillRepository;
 import com.example.demo.service.SkillService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SkillServiceImpl implements SkillService {
 
     private final SkillRepository repo;
@@ -18,42 +19,11 @@ public class SkillServiceImpl implements SkillService {
     @Override
     public Skill createSkill(Skill skill) {
 
-        if (skill.getMinCompetencyScore() < 0 || skill.getMinCompetencyScore() > 100) {
-            throw new IllegalArgumentException("Score must be between 0 and 100");
-        }
-
-        repo.findBySkillName(skill.getSkillName())
-                .ifPresent(s -> {
-                    throw new IllegalArgumentException("Skill name already exists");
-                });
+        repo.findByCode(skill.getCode()).ifPresent(s -> {
+            throw new IllegalArgumentException("Skill code must be unique");
+        });
 
         return repo.save(skill);
-    }
-
-    @Override
-    public Skill updateSkill(Long id, Skill update) {
-
-        Skill skill = repo.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Skill not found"));
-
-        skill.setSkillName(update.getSkillName());
-        skill.setCategory(update.getCategory());
-        skill.setDescription(update.getDescription());
-        skill.setMinCompetencyScore(update.getMinCompetencyScore());
-
-        return repo.save(skill);
-    }
-
-    @Override
-    public Skill getById(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Skill not found"));
-    }
-
-    @Override
-    public List<Skill> getAllSkills() {
-        return repo.findAll();
     }
 
     @Override
@@ -62,9 +32,8 @@ public class SkillServiceImpl implements SkillService {
     }
 
     @Override
-    public void deactivateSkill(Long id) {
-        Skill skill = getById(id);
-        skill.setActive(false);
-        repo.save(skill);
+    public Skill getById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Skill not found"));
     }
 }
