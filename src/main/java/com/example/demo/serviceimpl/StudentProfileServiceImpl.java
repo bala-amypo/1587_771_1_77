ed5ec7a14fbdc46ed2a79a77640f0c0e335f3026
bala-amypo <bala@amypo.in>
2025-12-26 17:52:57 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
@@ -3,23 +3,42 @@ package com.example.demo.serviceimpl;
 import com.example.demo.entity.StudentProfile;
 import com.example.demo.repository.StudentProfileRepository;
 import com.example.demo.service.StudentProfileService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class StudentProfileServiceImpl implements StudentProfileService {
+    private final StudentProfileRepository studentProfileRepository;
 
-    private final StudentProfileRepository repository;
+    public StudentProfileServiceImpl(StudentProfileRepository studentProfileRepository) {
+        this.studentProfileRepository = studentProfileRepository;
+    }
 
     @Override
     public StudentProfile createOrUpdateProfile(StudentProfile profile) {
-        return repository.save(profile);
+        return studentProfileRepository.save(profile);
+    }
+
+    @Override
+    public StudentProfile getProfileById(Long id) {
+        return studentProfileRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student profile not found in database")); // ResourceNotFoundException logic 
     }
 
     @Override
     public StudentProfile getByUserId(Long userId) {
-        return repository.findByUserId(userId)
-                .orElseThrow(() -> new RuntimeException("Profile not found"));
+        return studentProfileRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("profile not found")); 
+    }
+
+    @Override
+    public StudentProfile getProfileByEnrollmentId(String enrollmentId) {
+        return studentProfileRepository.findByEnrollmentId(enrollmentId)
+                .orElseThrow(() -> new RuntimeException("Enrollment not found"));
+    }
+
+    @Override
+    public List<StudentProfile> getAllProfiles() {
+        return studentProfileRepository.findAll();
     }
 }
