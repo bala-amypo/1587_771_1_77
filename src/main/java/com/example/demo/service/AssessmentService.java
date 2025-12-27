4b7fe1c8c620287aec0b2e4000320500165b6bbb
bala-amypo <bala@amypo.in>
2025-12-27 03:25:40 +0000

    User commit

--- src/main/java/com/example/demo/service/AssessmentService.java
+++ src/main/java/com/example/demo/service/AssessmentService.java
@@ -1,8 +1,21 @@
 
+// package com.example.demo.service;
+
+// import com.example.demo.entity.AssessmentResult;
+
+// public interface AssessmentService {
+//     AssessmentResult recordAssessment(AssessmentResult result);
+// }
+
+
+
 package com.example.demo.service;
 
 import com.example.demo.entity.AssessmentResult;
+import java.util.List;
 
 public interface AssessmentService {
     AssessmentResult recordAssessment(AssessmentResult result);
+    List<AssessmentResult> getResultsByStudent(Long studentId);
+    List<AssessmentResult> getResultsByStudentAndSkill(Long studentId, Long skillId);
 }
