9f414103b2f118479c034cfc3d1d6665becc9228
bala-amypo <bala@amypo.in>
2025-12-28 13:20:32 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,20 +1,34 @@
+
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import org.springframework.context.annotation.*;
+
+// @Configuration
+// public class OpenApiConfig {
+
+//     @Bean
+//     public OpenAPI openAPI() {
+//         return new OpenAPI();
+//     }
+// }
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9111.pro604cr.amypo.ai")
+                        new Server().url("https://9137.32procr.amypo.ai")
                 ));
-        }
+    }
 }
