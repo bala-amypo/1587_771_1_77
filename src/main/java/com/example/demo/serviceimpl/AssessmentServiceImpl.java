31ac268abf49094e448939efadfe677a1afd9b67
bala-amypo <bala@amypo.in>
2025-12-28 13:49:13 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
@@ -1,27 +1,27 @@
-package com.example.demo.serviceimpl;
+// package com.example.demo.serviceimpl;
 
-import com.example.demo.entity.AssessmentResult;
-import com.example.demo.repository.AssessmentResultRepository;
-import com.example.demo.service.AssessmentService;
-import org.springframework.stereotype.Service;
+// import com.example.demo.entity.AssessmentResult;
+// import com.example.demo.repository.AssessmentResultRepository;
+// import com.example.demo.service.AssessmentService;
+// import org.springframework.stereotype.Service;
 
-@Service //fix
-public class AssessmentServiceImpl implements AssessmentService{
+// @Service //fix
+// public class AssessmentServiceImpl implements AssessmentService{
 
-    private final AssessmentResultRepository repo;
+//     private final AssessmentResultRepository repo;
 
-    public AssessmentServiceImpl(AssessmentResultRepository repo) {
-        this.repo = repo;
-    }
+//     public AssessmentServiceImpl(AssessmentResultRepository repo) {
+//         this.repo = repo;
+//     }
 
-    public AssessmentResult recordAssessment(AssessmentResult r) {
-        if (r.getScore() == null ||
-            r.getScore() < 0 ||
-            r.getScore() > r.getMaxScore()) {
+//     public AssessmentResult recordAssessment(AssessmentResult r) {
+//         if (r.getScore() == null ||
+//             r.getScore() < 0 ||
+//             r.getScore() > r.getMaxScore()) {
 
-            throw new IllegalArgumentException("Score must be between 0 and 100");
-        }
+//             throw new IllegalArgumentException("Score must be between 0 and 100");
+//         }
 
-        return repo.save(r);
-    }
-}
+//         return repo.save(r);
+//     }
+// }
