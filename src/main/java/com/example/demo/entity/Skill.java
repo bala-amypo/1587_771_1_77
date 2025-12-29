1502d463fb606b433b763f9e4f188e40e15274cc
bala-amypo <bala@amypo.in>
2025-12-29 04:13:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/Skill.java
+++ src/main/java/com/example/demo/entity/Skill.java
@@ -28,3 +28,35 @@
 //     @Builder.Default
 //     private boolean active = true;
 // }
+
+
+
+
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String code; // or skillName
+
+    @Column(nullable = false)
+    private String category;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Double minCompetencyScore; // Range 0-100
+
+    @Builder.Default
+    private Boolean active = true;
+}
+
