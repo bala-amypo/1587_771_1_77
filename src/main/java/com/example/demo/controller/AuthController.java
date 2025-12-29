dcd66e9d58c860e5449d0ce65fa3fe32ffe0ccfe
bala-amypo <bala@amypo.in>
2025-12-29 03:36:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -39,6 +39,67 @@
 
 
 
+package com.example.demo.controller;
 
+import com.example.demo.config.JwtUtil;
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.LoginResponse;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    // REGISTER (PUBLIC)
+    @PostMapping("/register")
+    @Operation(summary = "Register new user")
+    public UserDTO register(@RequestBody User user) {
+        return toDTO(userService.register(user));
+    }
+
+    // LOGIN + RETURN TOKEN (PUBLIC)
+    @PostMapping("/login")
+    @Operation(summary = "User login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+
+        User user = userService.login(request.getUsername(), request.getPassword());
+
+        String token = jwtUtil.generateToken(user);
+
+        return new LoginResponse(
+                token,
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getRole().name()
+        );
+    }
+
+   
+
+    private UserDTO toDTO(User u) {
+        return UserDTO.builder()
+                .id(u.getId())
+                .username(u.getUsername())
+                .email(u.getEmail())
+                .role(u.getRole())
+                .build();
+    }
+}
 
 
